00d550aee74987cc29c4363c74d2cb1e99e3c44b
Luichix <luichix.rex@gmail.com>
2024-01-11 05:32:55 -0800

    Practice Collections

--- src/practice/Lists.java
+++ src/practice/Lists.java
@@ -1,8 +1,6 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class Lists {
 
@@ -12,6 +10,7 @@ public class Lists {
     };
 
     HashMap<String, String> map = new HashMap<>();
+    HashSet<Integer> numbers = new HashSet<>();
 
     public void elements(){
         myList.add("Father");
@@ -33,6 +32,60 @@ public class Lists {
         map.put("Guatemala", "Ciudad de Guatemala");
 
         System.out.println(map);
+
+        for (String item: map.keySet() ){
+            System.out.println("Key: " + item + " - " + map.get(item));
+        }
+
+        numbers.add(2);
+        numbers.add(7);
+        numbers.add(9);
+
+        for (int i = 0; i < 10; i ++){
+            if (numbers.contains(i)){
+                System.out.println("We found the value " + i);
+            } else {
+                System.out.println("We don't find the value " + i);
+            }
+        }
+
+        // Get an object iterator from the ArrayList with type iterator
+        Iterator<String> it = myList.iterator();
+
+        // Print the first item
+        System.out.println(it.next());
+
+        // Loop through a Collection
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        // Remove items from a Collection
+
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(6);
+        num.add(9);
+        num.add(12);
+        num.add(15);
+        num.add(20);
+
+        Iterator<Integer> itList = num.iterator();
+        while(itList.hasNext()){
+            Integer itValue = itList.next();
+
+            if(itValue < 10){
+                itList.remove();
+            }else{
+                System.out.println(itValue + " removed");
+            }
+
+        }
+
+        // num.removeIf(itValue -> itValue < 10);
+
+        System.out.println(num);
+
 
 
     }
