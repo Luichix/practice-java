1dc289fb908eaafcfaa3e148f47e70df442563ba
Luichix <luichix.rex@gmail.com>
2024-03-14 20:56:34 -0600

    practice(algorithm): two Sum

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
-import practice.Lists;
-import practice.Pig;
-import practice.Practice;
+import algorithm.Algorithm;
+
+import java.util.Arrays;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
     public static  void main (String[] args){
-        Practice practice = new Practice(2024, "Hummer");
 
-        practice.test();
+        // Params
+        int[] arrayInt = new int[5];
 
-        Pig porky = new Pig();
-        porky.animalName();
-        porky.animalSound();
-        porky.animalSleep();
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+        arrayInt[3] = 4;
+        arrayInt[4] = 5;
 
-        Lists lists = new Lists();
+        Algorithm algorithm = new Algorithm();
 
-        lists.elements();
+        System.out.println(Arrays.toString(algorithm.twoSum(arrayInt, 4)));
+        System.out.println(Arrays.toString(algorithm.twoSum2(arrayInt, 4)));
+
+
+        Algorithm.ListNode list1 = new Algorithm.ListNode(1, new Algorithm.ListNode(4, new Algorithm.ListNode(2)) );
+        algorithm.addTwoNumbers(list1, list1);
+
+
+
+
     }
 }
--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -1,56 +1,133 @@
 package algorithm;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Algorithm {
+    /**
+     * Given an array of integers numbs and an integer target, return indices of the two numbers such that they add up to target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * You can return the answer in any order.
+     * Example 1:
+     * Input: numbs = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Explanation: Because numbs[0] + numbs[1] == 9, we return [0, 1].
+     * Example 2:
+     * Input: numbs = [3,2,4], target = 6
+     * Output: [1,2]
+     * Example 3:
+     * Input: numbs = [3,3], target = 6
+     * Output: [0,1]
+     */
+    public int[] twoSum (int[] numbers, int target){
 
-    public static void main(String[] args){
-        // Create an object ArrayList
-        ArrayList<Integer> arr = new ArrayList<>();
+        /*
+        *   Using a brute force method to solve this problem
+        *   1. Run through the list from the second element until the last element
+        *   2. Review if the sum of the current element with the others element is equal to target value
+        *   3. If the element is equal at target value, save both indexes into the list and return
+        *   4. Is not equal at target, continue with the next loop
+        *  */
 
-        // Add values into ArrayList arr
-        arr.add(6);
-        arr.add(8);
-        arr.add(2);
-        arr.add(4);
-        arr.add(9);
-        arr.add(5);
-        arr.add(1);
+        int[] sum = new int[2];
 
-        // Print in console before sort array
-        System.out.println(arr);
+        System.out.println("Two Sum");
+        for(int i = 1; i< numbers.length ;i++){
+            for (int j = 0; j < numbers.length; j++){
+                if(j != i){
+                    if(target == numbers[i] + numbers[j]){
+                        sum[0] = i;
+                        sum[1] = j;
+                        return sum;
+                    }
+                }
 
-        // Sort array with static method Insertion Sort
-        insertionSort(arr);
-
-        // Print in console after sort array
-        System.out.println(arr);
-    }
-
-    public static void insertionSort (ArrayList<Integer> arr){
-        /* Run through the object ArrayList arr
-        * Define i as counter of element in arr.
-        * Value i start with one.
-        * Run of index 1 to last element in arr.
-        * */
-        for (int i = 1; i < arr.size(); i++  ){
-            // Define j as a second counter of elements in arr with the same value of i.
-            int j = i;
-
-            // Do loop while counter j is not less than 0, and while...
-            // ...the element in the current position is less than the before position.
-            while((j > 0) && (arr.get(j) < arr.get(j - 1))){
-                // Change the booth element positions.
-                swap(arr, j);
-                // Reduce the value of counter j for compare the all before values in the next while loops.
-                j = j-1;
             }
+
+
+        };
+
+        return sum;
+
+    };
+
+    public int[] twoSum2 (int[] numbers, int target){
+        /*
+        *   Using a efficient method to solve this problem
+        *   1. First review all the values of the list
+        *   2. Think in we have 2 values the target value, and the index value.
+        *   3. We calculate the value necessary to get a rest value. We need to rest the current value of the target value.
+        *   4, With that result search into the indexes if exist the required value
+        *   5. If no exists then review the nex value.
+        *  */
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] sum = new int[2];
+
+        for (int i = 0; i < numbers.length; i++){
+            int restValue = target - numbers[i];
+
+            if (map.containsKey(restValue)){
+                sum[0] = i;
+                sum[1] = map.get(restValue);
+                return sum;
+            }else {
+                map.put( numbers[i], i);
+            }
+
         }
+
+
+        return sum;
     }
 
-    public static void swap(ArrayList<Integer> arr, int position){
-        int value = arr.get(position - 1);
-        arr.set(position - 1, arr.get(position));
-        arr.set(position, value);
+
+    /*
+    * You are given two non-empty linked lists representing two non-negative integers.
+    * The digits are stored in reverse order, and each of their nodes contains a single digit.
+    * Add the two numbers and return the sum as a linked list.
+    * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    * Example 1:
+    * Input: l1 = [2,4,3], l2 = [5,6,4]
+    * Output: [7,0,8]
+    * Explanation: 342 + 465 = 807.
+    * Example 2:
+    * Input: l1 = [0], l2 = [0]
+    * Output: [0]
+    * Example 3:
+    * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    * Output: [8,9,9,9,0,0,0,1]
+    *  */
+
+    public static class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         public ListNode(int val) { this.val = val; }
+         public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+
+    public static void getIntFromListNode(ListNode list){
+
+        System.out.println("getFromListNode");
+    }
+
+
+
+    public void addTwoNumbers(ListNode list1, ListNode list2){
+            /*
+            *   Using a Brute Force Method:
+            *   1. First we need extract numbers from the listNode
+            *   2. We use a method to reorder the digits into numbers
+            *   3. Sum both values
+            *   4. Reorder as a Linked List the received value
+            * */
+
+        getIntFromListNode(list1);
+        getIntFromListNode(list2);
+
+    }
+
 }
